23e6521de29f4ab638389d7a101a233864f3e2f3
shiro-kreishi <vadas25@yandex.ru>
2024-07-08 20:25:21 +0500

    add email field to list display

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -8,7 +8,7 @@ User = get_user_model()
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'first_name', 'last_name', )
+    list_display = ('email', 'username', 'first_name', 'last_name', )
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         ('Personal info', {'fields': ('first_name', 'last_name', )}),
