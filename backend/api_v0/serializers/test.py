87a0f9a50653ce78987dbddbc17814255f12d7c3
shiro-kreishi <vadas25@yandex.ru>
2024-07-17 14:00:30 +0500

    update serializers

---
+++ backend/api_v0/serializers/test.py
@@ -0,0 +1,80 @@
+from rest_framework import serializers
+
+from api_v0.serializers import ExerciseSerializer
+from testing.models.test import \
+    Test, DoctorToTest, Whitelist, PublicTest, \
+    ResponseTest, Exercise, ResponseExercise, ExerciseToTest
+from users.models import User
+
+
+class TestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Test
+        fields = ['id', 'name', 'description']
+
+
+class TestDetailSerializer(serializers.ModelSerializer):
+    exercises = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Test
+        fields = ['id', 'name', 'description', 'exercises']
+
+    def get_exercises(self, obj):
+        exercises = Exercise.objects.filter(exercisetotest__test=obj)
+        return ExerciseSerializer(exercises, many=True).data
+
+
+class TestCreateUpdateSerializer(serializers.ModelSerializer):
+    exercises = serializers.PrimaryKeyRelatedField(queryset=Exercise.objects.all(), many=True)
+
+    class Meta:
+        model = Test
+        fields = ['id', 'name', 'description', 'exercises']
+
+    def create(self, validated_data):
+        exercises = validated_data.pop('exercises')
+        test = Test.objects.create(**validated_data)
+        for exercise in exercises:
+            ExerciseToTest.objects.create(test=test, exercise=exercise)
+        return test
+
+    def update(self, instance, validated_data):
+        exercises = validated_data.pop('exercises')
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.save()
+
+        # Удаление старых связей
+        ExerciseToTest.objects.filter(test=instance).delete()
+        # Добавление новых связей
+        for exercise in exercises:
+            ExerciseToTest.objects.create(test=instance, exercise=exercise)
+        return instance
+
+
+class WhitelistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Whitelist
+        fields = ['id', 'user', 'test']
+
+
+class PublicTestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PublicTest
+        fields = ['id', 'test']
+
+
+class PublicDetailSerializer(serializers.ModelSerializer):
+    test = TestDetailSerializer()
+    class Meta:
+        model = PublicTest
+        fields = ['id', 'test']
+
+
+class ResponseTestSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=False, allow_null=True)
+
+    class Meta:
+        model = ResponseTest
+        fields = ['id', 'test', 'user', 'json_result']
--- backend/api_v0/views/exercise.py
+++ backend/api_v0/views/exercise.py
@@ -5,8 +5,18 @@ from project.settings import DEBUG as debug_settings
 from rest_framework import generics, mixins, views, permissions, viewsets
 
 from testing.models.test import Exercise, ExerciseToTest, ResponseExercise, ExerciseType
-from testing.serializers.testing import ExerciseSerializer, ResponseExerciseSerializer, \
-    PublicDetailSerializer, ExerciseToTestSerializer, ExerciseTypeSerializer
+from api_v0.serializers import ResponseExerciseSerializer, \
+    PublicDetailSerializer
+from api_v0.serializers import ExerciseSerializer, ExerciseTypeSerializer, ExerciseToTestSerializer, \
+    ExerciseTypeSerializer, ExerciseUpdateOrCreateSerializer
+
+
+class IsSuperUserOrDoctor(IsMemberOfGroupOrAdmin):
+    group_name = 'Doctors'
+
+
+class IsSuperUserOrAdmin(IsMemberOfGroupOrAdmin):
+    group_name = 'Administrators'
 
 
 class ExerciseTypeViewSet(viewsets.ModelViewSet):
@@ -21,7 +31,22 @@ class ExerciseModelViewSet(mixins.CreateModelMixin,
                            mixins.ListModelMixin,
                            GenericViewSet):
     queryset = Exercise.objects.all()
-    serializer_class = ExerciseSerializer
+    # serializer_class = ExerciseSerializer
+    def get_serializer_class(self):
+        if debug_settings:
+            print(f'action: {self.action}')
+        if self.action in ['create', 'update', 'partial_update']:
+            return ExerciseUpdateOrCreateSerializer
+        return ExerciseSerializer
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            permission_classes = [permissions.AllowAny]
+        elif self.action in ['create', 'update', 'partial_update', 'destroy']:
+            permission_classes = [IsSuperUserOrAdmin, IsSuperUserOrDoctor]
+        else:
+            permission_classes = [permissions.IsAuthenticated]
+        return [permission() for permission in permission_classes]
 
 
 class ExerciseToTestModelViewSet(mixins.CreateModelMixin,
@@ -32,6 +57,9 @@ class ExerciseToTestModelViewSet(mixins.CreateModelMixin,
                                  GenericViewSet):
     queryset = ExerciseToTest.objects.all()
     serializer_class = ExerciseToTestSerializer
+    permission_classes = [
+        IsSuperUserOrAdmin, IsSuperUserOrDoctor
+    ]
 
 
 class ResponseExerciseModelViewSet(mixins.CreateModelMixin,
