8a1c6792d89e6dcbd8583693e63a83daee7a1961
shiro-kreishi <vadas25@yandex.ru>
2024-07-26 20:19:01 +0500

    add userid serializer

--- backend/user_api/serializers/user.py
+++ backend/user_api/serializers/user.py
@@ -48,6 +48,12 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('email', 'username', 'last_name', 'first_name',)
 
 
+class UserWithIdSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = ('id', 'email', 'username', 'last_name', 'first_name',)
+
+
 class ChangePasswordSerializer(serializers.Serializer):
     old_password = serializers.CharField(required=True)
     new_password = serializers.CharField(required=True)
