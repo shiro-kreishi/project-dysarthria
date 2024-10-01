557ecf9cacc1bf2db5e366de621f24d675b4019c
XEDDOM <gugl.osnova@gmail.com>
2024-10-01 09:51:26 +0500

    Added 404 page on invalid password recovery token, changed redirection on password recovery to notification page and changed text on notification page

--- frontend/src/Pages/Components/PasswordConfirmation.js
+++ frontend/src/Pages/Components/PasswordConfirmation.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, Link } from 'react-router-dom';
 import {
   Box,
   Button,
@@ -13,6 +13,8 @@ import {
   InputRightElement,
   InputGroup,
   useColorModeValue,
+  Heading,
+  Text,
 } from "@chakra-ui/react";
 import axiosConfig from './AxiosConfig';
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
@@ -26,6 +28,7 @@ const PasswordConfirmation = () => {
   const [error, setError] = useState('');
   const url = "/api/user/forgot-password-change/";
   let { passwordConfirmKey } = useParams();
+  const [isAllowed, setIsAllowed] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -34,6 +37,9 @@ const PasswordConfirmation = () => {
         try {
           const response = await axiosConfig.get(`${url}${passwordConfirmKey}/`);
           console.log(response);
+          if (response.status === 200) {
+            setIsAllowed(true);
+          }
         }
         catch (e) {
           console.log(e);
@@ -79,45 +85,63 @@ const PasswordConfirmation = () => {
     <Flex minH={'100vh'} align={'center'} justify={'center'} bg={useColorModeValue('gray.50', 'gray.800')}>
       <Stack spacing={8} mx={'auto'} maxW={'lg'} w={'full'} py={12} px={6}>
         <Box rounded={'lg'} bg={useColorModeValue('white', 'gray.700')} boxShadow={'lg'} p={8}>
-          <Stack spacing={4} >
-            <form onSubmit={handleSubmit}>
-              <FormControl id="formBasicCode" mb="4">
-                <FormLabel>Код из письма на Email</FormLabel>
-                <Input type="text" placeholder="Введите код" value={code} onChange={e => setCode(e.target.value)} />
-              </FormControl>
-              <FormControl id="formBasicPassword1" mb="4">
-                <FormLabel>Пароль</FormLabel>
-                <InputGroup>
-                  <Input type={showPassword1 ? 'text' : 'password'} placeholder="Введите пароль" value={password1} onChange={e => setPassword1(e.target.value)} />
-                  <InputRightElement h={'full'}>
-                    <Button variant={'ghost'} onClick={() => setShowPassword1((showPassword1) => !showPassword1)}>
-                      {showPassword1 ? <ViewIcon /> : <ViewOffIcon />}
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-              </FormControl>
-              <FormControl id="formBasicPassword2" mb="4">
-                <FormLabel>Подтверждение пароля</FormLabel>
-                <InputGroup>
-                  <Input type={showPassword2 ? 'text' : 'password'} placeholder="Подтвердите пароль" value={password2} onChange={e => setPassword2(e.target.value)} />
-                  <InputRightElement h={'full'}>
-                    <Button variant={'ghost'} onClick={() => setShowPassword2((showPassword2) => !showPassword2)}>
-                      {showPassword2 ? <ViewIcon /> : <ViewOffIcon />}
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-              </FormControl>
-              <Stack spacing={10}>
-                {error && (
-                  <Alert status="error" mb="4">
-                    <AlertIcon />
-                    {error}
-                  </Alert>
-                )}
-                <Button type="submit" bg={'blue.400'} color={'white'} _hover={{ bg: 'blue.500', }}>Применить</Button>
-              </Stack>
-            </form>
-          </Stack>
+          {!isAllowed ? 
+            <Box textAlign="center" py={10} px={6}>
+              <Heading display="inline-block" as="h2" size="2xl" bgGradient="linear(to-r, teal.400, teal.600)" backgroundClip="text">
+                404
+              </Heading>
+              <Text fontSize="18px" mt={3} mb={2}>
+                Страница не найдена
+              </Text>
+              <Text color={'gray.500'} mb={6}>
+                Страница, которую вы ищете, не существует
+              </Text>
+              <Link to="/">
+                <Button colorScheme="teal" bgGradient="linear(to-r, teal.400, teal.500, teal.600)" color="white" variant="solid">
+                  Вернуться на главную страницу
+                </Button>
+              </Link>
+            </Box> :
+            <Stack spacing={4} >
+              <form onSubmit={handleSubmit}>
+                <FormControl id="formBasicCode" mb="4">
+                  <FormLabel>Код из письма на Email</FormLabel>
+                  <Input type="text" placeholder="Введите код" value={code} onChange={e => setCode(e.target.value)} />
+                </FormControl>
+                <FormControl id="formBasicPassword1" mb="4">
+                  <FormLabel>Пароль</FormLabel>
+                  <InputGroup>
+                    <Input type={showPassword1 ? 'text' : 'password'} placeholder="Введите пароль" value={password1} onChange={e => setPassword1(e.target.value)} />
+                    <InputRightElement h={'full'}>
+                      <Button variant={'ghost'} onClick={() => setShowPassword1((showPassword1) => !showPassword1)}>
+                        {showPassword1 ? <ViewIcon /> : <ViewOffIcon />}
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
+                </FormControl>
+                <FormControl id="formBasicPassword2" mb="4">
+                  <FormLabel>Подтверждение пароля</FormLabel>
+                  <InputGroup>
+                    <Input type={showPassword2 ? 'text' : 'password'} placeholder="Подтвердите пароль" value={password2} onChange={e => setPassword2(e.target.value)} />
+                    <InputRightElement h={'full'}>
+                      <Button variant={'ghost'} onClick={() => setShowPassword2((showPassword2) => !showPassword2)}>
+                        {showPassword2 ? <ViewIcon /> : <ViewOffIcon />}
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
+                </FormControl>
+                <Stack spacing={10}>
+                  {error && (
+                    <Alert status="error" mb="4">
+                      <AlertIcon />
+                      {error}
+                    </Alert>
+                  )}
+                  <Button type="submit" bg={'blue.400'} color={'white'} _hover={{ bg: 'blue.500', }}>Применить</Button>
+                </Stack>
+              </form>
+            </Stack>
+          }
         </Box>
       </Stack>
     </Flex>
